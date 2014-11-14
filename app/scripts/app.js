8f47c0c6612cccbf215b26fcff955056ee8b3db5
Tobias Haaß <tobias.haass@andrena.de>
2014-11-14 10:08:38 +0100

    Initiale Projektstruktur mit Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name alertDirectiveApp
+ * @description
+ * # alertDirectiveApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('alertDirectiveApp', []);
